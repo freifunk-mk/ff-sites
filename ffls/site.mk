7576abfb5613271d910801686b48649b3c581155
Alexander <alex@tempx.de>
2016-09-20 22:59:51 +0200

    First build successfull.

--- ffls/site.mk
+++ ffls/site.mk
@@ -3,7 +3,6 @@
 ##	GLUON_SITE_PACKAGES
 #		specify gluon/openwrt packages to include here
 #		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
-
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-alfred \
@@ -26,10 +25,46 @@ GLUON_SITE_PACKAGES := \
 	gluon-radvd \
 	gluon-setup-mode \
 	gluon-status-page \
-	gluon-txpowerfix \
 	haveged \
 	iptables \
 	iwinfo
+
+#	Additional packages used in the domain
+GLUON_SITE_PACKAGES += \
+	gluon-luci-private-wifi \
+	gluon-luci-node-role \
+	gluon-ebtables-filter-roguenets \
+
+#	Target-depending packages used in the domain
+ifeq ($(GLUON_TARGET),$(filter $(GLUON_TARGET), ar71xx-generic ar71xx-nand mpc85xx-generic))
+GLUON_SITE_PACKAGES += \
+	gluon-aptimeclock \
+	gluon-ssid-changer \
+        gluon-txpowerfix \
+        gluon-forceradioenable \
+	gluon-weeklyreboot
+endif
+
+ifeq ($(GLUON_TARGET),$(filter $(GLUON_TARGET), x86-generic x86-64))
+GLUON_SITE_PACKAGES += \
+	kmod-hyperv-balloon \
+	kmod-hyperv-net-vsc \
+	kmod-hyperv-util \
+	kmod-hyperv-storage \
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether \
+        kmod-sky2 \
+        kmod-r8169 \
+        kmod-forcedeth \
+        kmod-8139too \
+	kmod-atl2
+endif
+
+
 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
@@ -37,7 +72,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 16.09.20
 
 
 ##	GLUON_RELEASE
@@ -54,4 +89,5 @@ GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 GLUON_PRIORITY ?= 0
 
 # Languages to include
-GLUON_LANGS ?= en de
+GLUON_LANGS ?= de
+
--- ffsm/site.mk
+++ ffsm/site.mk
@@ -1,40 +1,93 @@
-GLUON_SITE_PACKAGES := \
-        gluon-mesh-batman-adv-15 \
-        gluon-alfred \
-        gluon-announced \
-        gluon-autoupdater \
-        gluon-setup-mode \
-        gluon-config-mode-core \
-        gluon-config-mode-autoupdater \
-        gluon-config-mode-hostname \
-        gluon-config-mode-mesh-vpn \
-        gluon-config-mode-geo-location \
-        gluon-config-mode-contact-info \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-filter-ra-dhcp \
-        gluon-luci-admin \
-        gluon-luci-autoupdater \
-        gluon-luci-portconfig \
-        gluon-luci-private-wifi \
-        gluon-luci-wifi-config \
-        gluon-next-node \
-        gluon-mesh-vpn-fastd \
-        gluon-radvd \
-        gluon-status-page \
-        iwinfo \
-        iptables \
-        haveged
+##	gluon site.mk makefile example
 
-DEFAULT_GLUON_RELEASE := 15.07.1
+##	GLUON_SITE_PACKAGES
+#		specify gluon/openwrt packages to include here
+#		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-alfred \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	haveged \
+	iptables \
+	iwinfo
+
+#	Additional packages used in the domain
+GLUON_SITE_PACKAGES += \
+	gluon-luci-private-wifi \
+	gluon-luci-node-role \
+	gluon-ebtables-filter-roguenets \
+
+#	Target-depending packages used in the domain
+ifeq ($(GLUON_TARGET),$(filter $(GLUON_TARGET), ar71xx-generic ar71xx-nand mpc85xx-generic))
+GLUON_SITE_PACKAGES += \
+	gluon-aptimeclock \
+	gluon-ssid-changer \
+        gluon-txpowerfix \
+        gluon-forceradioenable \
+	gluon-weeklyreboot
+endif
+
+ifeq ($(GLUON_TARGET),$(filter $(GLUON_TARGET), x86-generic x86-64))
+GLUON_SITE_PACKAGES += \
+	kmod-hyperv-balloon \
+	kmod-hyperv-net-vsc \
+	kmod-hyperv-util \
+	kmod-hyperv-storage \
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether \
+        kmod-sky2 \
+        kmod-r8169 \
+        kmod-forcedeth \
+        kmod-8139too \
+	kmod-atl2
+endif
+
+
+
+##	DEFAULT_GLUON_RELEASE
+#		version string to use for images
+#		gluon relies on
+#			opkg compare-versions "$1" '>>' "$2"
+#		to decide if a version is newer or not.
+
+DEFAULT_GLUON_RELEASE := 16.09.20
+
+
+##	GLUON_RELEASE
+#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#		e.g.:
+#			$ make images GLUON_RELEASE=23.42+5
+#		would generate images named like this:
+#			gluon-ff%site_code%-23.42+5-%router_model%.bin
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
+# Default priority for updates.
 GLUON_PRIORITY ?= 0
-GLUON_BRANCH ?= stable
-export GLUON_BRANCH
 
-GLUON_TARGET ?= ar71xx-generic
-export GLUON_TARGET
-
+# Languages to include
 GLUON_LANGS ?= de
+
