871ed2d245c44482df68df19a0072089ac02023e
Skymaker <lukas@skymaker.info>
2021-12-20 20:04:22 +0100

    Update ffis to gluon 2017.1.8

--- ffis/site.mk
+++ ffis/site.mk
@@ -1,24 +1,56 @@
-GLUON_FEATURES := \
-	autoupdater \
-	ebtables-filter-multicast \
-	ebtables-filter-ra-dhcp \
-	ebtables-limit-arp \
-	mesh-batman-adv-15 \
-	mesh-vpn-fastd \
-	radvd \
-	respondd \
-	status-page \
-	web-advanced \
-	web-wizard
+##	gluon site.mk makefile example
 
-GLUON_SITE_PACKAGES := haveged iwinfo
+##	GLUON_SITE_PACKAGES
+#		specify Gluon/LEDE packages to include here
 
-DEFAULT_GLUON_RELEASE := 19.06
+GLUON_SITE_PACKAGES := \
+	gluon-alfred \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-network \
+	gluon-web-wifi-config \
+	gluon-mesh-batman-adv-15 \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	haveged \
+	iwinfo
 
-GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+##	DEFAULT_GLUON_RELEASE
+#		version string to use for images
+#		gluon relies on
+#			opkg compare-versions "$1" '>>' "$2"
+#		to decide if a version is newer or not.
 
+DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
+
+# Variables set with ?= can be overwritten from the command line
+
+##	GLUON_RELEASE
+#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#		e.g.:
+#			$ make images GLUON_RELEASE=23.42+5
+#		would generate images named like this:
+#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+
+GLUON_RELEASE ?= 21.12
+
+# Default priority for updates.
 GLUON_PRIORITY ?= 0
 
+# Region code required for some images; supported values: us eu
 GLUON_REGION ?= eu
 
-GLUON_LANGS ?= de
+# Languages to include
+GLUON_LANGS ?= en de
