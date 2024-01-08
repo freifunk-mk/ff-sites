f7828d5937351400de1e13ff828a0738d7823270
skymaker-c2is <63470599+skymaker-c2is@users.noreply.github.com>
2024-01-08 19:28:31 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,37 +1,4 @@
 ##	gluon site.mk makefile example
-
-##	GLUON_FEATURES
-#		Specify Gluon features/packages to enable;
-#		Gluon will automatically enable a set of packages
-#		depending on the combination of features listed
-
-GLUON_FEATURES := \
-	autoupdater \
-	config-mode-mesh-vpn \
-	config-mode-domain-select \
-	ebtables-filter-multicast \
-	ebtables-filter-ra-dhcp \
-	ebtables-limit-arp \
-	mesh-batman-adv-15 \
-	respondd \
-	status-page \
-	web-advanced \
-	web-wizard 
-## gluon airtime
-## autoupdater wifi falback
-##	GLUON_SITE_PACKAGES
-#		Specify additional Gluon/OpenWrt packages to include here;
-#		A minus sign may be prepended to remove a packages from the
-#		selection that would be enabled by default or due to the
-#		chosen feature flags
-
-GLUON_SITE_PACKAGES := \
-	iwinfo \
-	ffac-autoupdater-wifi-fallback \
-	respondd-module-airtime \
-	ffac-weeklyreboot \
-	ffmuc-ipv6-ra-filter \
-	ffmuc-mesh-vpn-wireguard-vxlan
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
 #		gluon relies on
