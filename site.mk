81847f48f0ab345aeda6cc66eba790e0a750f8e8
Skymaker <lukas@skymaker.info>
2024-06-03 19:24:48 +0200

    beta 24.05.1

--- site.mk
+++ site.mk
@@ -1,44 +1,17 @@
 ##	gluon site.mk makefile example
 
-##	GLUON_FEATURES
-#		Specify Gluon features/packages to enable;
-#		Gluon will automatically enable a set of packages
-#		depending on the combination of features listed
+##	GLUON_MULTIDOMAIN
+#		Build gluon with multidomain support.
 
-GLUON_FEATURES := \
-	autoupdater \
-	config-mode-mesh-vpn \
-	config-mode-domain-select \
-	ebtables-filter-multicast \
-	ebtables-filter-ra-dhcp \
-	ebtables-limit-arp \
-	mesh-batman-adv-15 \
-	respondd \
-	status-page \
-    mesh-vpn-wireguard-vxlan \
-	web-advanced \
-	web-wizard 
-## gluon airtime
-## autoupdater wifi falback
-##	GLUON_SITE_PACKAGES
-#		Specify additional Gluon/OpenWrt packages to include here;
-#		A minus sign may be prepended to remove a packages from the
-#		selection that would be enabled by default or due to the
-#		chosen feature flags
+GLUON_MULTIDOMAIN=1
 
-GLUON_SITE_PACKAGES := \
-	iwinfo \
-	ffho-autoupdater-wifi-fallback \
-	respondd-module-airtime 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
 #		gluon relies on
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-GLUON_MULTIDOMAIN=1
-
-DEFAULT_GLUON_RELEASE := v2021.12.6-FFMK-BETA+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 24.05.1
 
 # Variables set with ?= can be overwritten from the command line
 
@@ -60,5 +33,4 @@ GLUON_REGION ?= eu
 # Languages to include
 GLUON_LANGS ?= en de
 
-# Do not build images for deprecated devices
 GLUON_DEPRECATED ?= 1
